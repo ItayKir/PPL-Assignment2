3605d8e416eca2f0b8792a40076b5a8a8db5eaae
Itay Kigner <itaykirgner@gmail.com>
2026-05-24 16:37:35 +0300

    Renamed tests and started working on 2.c

--- L3/SyntacticTransformation.ts
+++ L3/SyntacticTransformation.ts
@@ -1,14 +1,36 @@
-import { ClassExp, ProcExp, Exp, Program } from "./L3-ast";
+import { ClassExp, ProcExp, Exp, Program, Binding, 
+    makeProcExp, makeVarDecl, makeVarRef, makeIfExp, 
+    makeAppExp, makePrimOp, makeLitExp} from "./L3-ast";
 import { Result, makeFailure } from "../shared/result";
+
+import { reduceRight } from "ramda";
+import { makeSymbolSExp } from "./L3-value";
 
 /*
 Purpose: Transform ClassExp to ProcExp
 Signature: class2proc(classExp)
 Type: ClassExp => ProcExp
 */
-export const class2proc = (exp: ClassExp): ProcExp =>
-    //@TODO
-    exp;
+export const class2proc = (exp: ClassExp): ProcExp =>{
+    const baseError = makeLitExp(makeSymbolSExp("error"));
+
+    const methods = exp.methods.reduceRight((acc: Exp, method: Binding) => {
+        
+        const testCondition = makeAppExp(
+            makePrimOp("eq?"),
+            [makeVarRef("msg"), makeLitExp(makeSymbolSExp(method.var.var))]
+        );
+
+        const thenBody = (method.val as ProcExp).body[0];
+
+        return makeIfExp(testCondition, thenBody, acc);
+        
+    }, baseError);
+
+    const innerFunction = makeProcExp([makeVarDecl("msg")], [methods]);
+
+    return makeProcExp(exp.fields, [innerFunction]);
+}
 
 
 /*
