674df4e3be7919328a73412907da28458bb39407
Itay Kigner <itaykirgner@gmail.com>
2026-05-25 15:29:57 +0300

    Changed comments

--- q3.ts
+++ q3.ts
@@ -19,13 +19,13 @@ export const l2ToPython = (exp: Exp | Program): Result<string> =>
                      bind(l2ToPython(exp.alt), (alt: string) =>
                       makeOk(`(${then} if ${test} else ${alt})`)))) :
     isProcExp(exp) ? bind(l2ToPython(exp.body[0]), (body: string) => 
-                      makeOk(`(lambda ${exp.args.map(a => a.var).join(', ')}: ${body})`.replace("lambda :", "lambda:"))) :
+                      makeOk(`(lambda ${exp.args.map(a => a.var).join(',')} : ${body})`.replace("lambda :", "lambda:"))) :
     isAppExp(exp) ? (
         isPrimOp(exp.rator) ? 
             bind(mapResult(l2ToPython, exp.rands), (rands: string[]) => makeOk(convertAppPrimOp((exp.rator as PrimOp).op, rands))) :
             bind(l2ToPython(exp.rator), (rator: string) => 
              bind(mapResult(l2ToPython, exp.rands), (rands: string[]) => 
-              makeOk(`${rator}(${rands.join(', ')})`)))
+              makeOk(`${rator}(${rands.join(',')})`)))
     ) :
     makeFailure(`Unknown expression: ${exp}`);
 
@@ -34,18 +34,15 @@ const convertPrimOp = (op: string): string => {
     switch (op) {
         case "=":
         case "eq?": return "==";
-        case "and": return "and";
-        case "or": return "or";
-        case "not": return "not";
         case "boolean?": return "(lambda x : (type(x) == bool))";
         case "number?": return "(lambda x : (type(x) == int or type(x) == float))";
         case "symbol?":
         case "string?": return "(lambda x : (type(x) == str))";
-        default: return op; // Fallback for +, -, *, /, <, >
+        default: return op; // +, -, *, /, <, >
     }
 }
 
-// Helper to format an application of a primitive operator
+// Helper to format primitive operator
 const convertAppPrimOp = (op: string, rands: string[]): string => {
     if (op === "not") {
         return `(not ${rands[0]})`;
@@ -54,7 +51,7 @@ const convertAppPrimOp = (op: string, rands: string[]): string => {
     } else if (op === "-" && rands.length === 1) { // Handling unary minus e.g. (- 5)
         return `(-${rands[0]})`;
     } else {
-        // Typical binary operators (+, -, *, /, <, >, ==, and, or)
+        // (+, -, *, /, <, >, ==, and, or)
         return `(${rands.join(` ${convertPrimOp(op)} `)})`;
     }
 }
